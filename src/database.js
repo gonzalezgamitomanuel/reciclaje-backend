22b64e2024f446e238543ffd14462adf37a69c41
Manuel Gonzalez Gamito <mgonzalez.gamito@sevilla.servinformgroup.local>
2021-05-26 10:04:38 +0200

    terc commit

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/angular-auth', {
+mongoose.connect('mongodb+srv://manu:mimanuel1@cluster0.gdshf.mongodb.net/ejemplo?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
